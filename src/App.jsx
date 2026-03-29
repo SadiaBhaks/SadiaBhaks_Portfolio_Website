8ed636e4ae37a25bb1f0a9748d8dd2ab9c7a38fb
SadiaBhaks <bhakssadia@gmail.com>
2026-03-29 19:28:50 +0600

    Fix case-sensitive import for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
-import Home from "./Components/home";
+import Home from "./Components/Home";
 import About from "./Components/About";
 import Skills from "./Components/Skills";
 import Projects from "./Components/Projects";
